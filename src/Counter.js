84a8a04c85edcc3d4a04f859ca8b955e4c826771
alf <alfmohenu@gmail.com>
2020-03-22 00:13:12 +0300

    Local storage functionality refactored into a hook

--- src/Counter.js
+++ src/Counter.js
@@ -1,37 +1,34 @@
 /* eslint-disable */
 import React, { useState, useEffect } from 'react';
 
-const getStateFromLocalStorage = () => {
-  const storage = localStorage.getItem('counterState');
-  if (storage) return JSON.parse(storage).count;
-  return 0;
-};
-const storeStateInLocalStorage = count => {
-  localStorage.setItem('counterState', JSON.stringify({ count }));
-  // console.log(localStorage);
+// const getStateFromLocalStorage = () => {
+//   const storage = localStorage.getItem('counterState');
+//   if (storage) return JSON.parse(storage).count;
+//   return 0;
+// };
+// const storeStateInLocalStorage = count => {
+//   localStorage.setItem('counterState', JSON.stringify({ count }));
+//   // console.log(localStorage);
+// };
+
+const useLocalStorage = (initialState, key) => {
+  const get = () => {
+    const storage = localStorage.getItem(key);
+    if (storage) return JSON.parse(storage).value;
+    return initialState;
+  };
+
+  const [value, setValue] = useState(get());
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify({ value }));
+  }, [value]);
+
+  return [value, setValue];
 };
 
 const Counter = ({ max, step }) => {
-  const [count, setCount] = React.useState(getStateFromLocalStorage());
-
-  const inc = val => val + 1;
-
-  // passing a function into the state updater will
-  // call the function the number of times called.
-
-  // Eg. This will call the function 3 times
-  // const increment = () => {
-  //   setCount(inc);
-  //   setCount(inc);
-  //   setCount(inc);
-  // };
-
-  // Not passing a func will call the state updater only once.
-  // const increment = () => {
-  //   setCount(count + 1);
-  //   setCount(count + 1);
-  //   setCount(count + 1);
-  // };
+  const [count, setCount] = useLocalStorage(0, 'countKey');
 
   const increment = () => {
     setCount(c => {
@@ -52,9 +49,9 @@ const Counter = ({ max, step }) => {
   }, [count]); // if 'count' changes, re-run useEffect()
 
   // or multiple useEffect
-  useEffect(() => {
-    storeStateInLocalStorage(count);
-  }, [count]);
+  // useEffect(() => {
+  //   storeStateInLocalStorage(count);
+  // }, [count]);
   return (
     <main className="Counter">
       <p className="count">{count}</p>
